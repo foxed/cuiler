c4d455567960d34d6267d246932457f36e4e77ce
Avi Gilligan <euraldius@gmail.com>
2014-04-04 14:02:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuiler::Application.config.secret_key_base = '733e05bc54679fd4fa0d6a901007cf1fdb5b9652431ba5656787d83517847cbc54b3c7210188f6706f29b58fee3b285622068c495fbb3edd57fd4a8b513bf677'
